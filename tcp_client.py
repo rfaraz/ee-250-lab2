069eb5c9bf059d47162bfb14f2f9c0e6ea458058
chompers98 <leyaageo@usc.edu>
2025-09-16 13:40:50 -0700

    added comments

--- tcp_client.py
+++ tcp_client.py
@@ -8,27 +8,20 @@ use python "input->" function, enter a line of a few letters, such as "abcd"
 import socket
 
 def main():
-
-    HOST = "127.0.0.1"    # The remote host
-    PORT = 12345              # The same port as used by the server
+     # TODO: Create a socket and connect it to the server at the designated IP and port
+    HOST = "172.20.10.5" # designated IP
+    PORT = 12345 # designated port
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT)) # connect socket to server at designated IP and port
-        message = input("Enter what you want to send over the TCP socket: ") # get user input to send
+
+        # TODO: Get user input and send it to the server using your TCP socket
+        message = input("Enter what you want to send over the TCP socket: ")
         s.sendall(message.encode("utf-8")) # send the message in bytes
+    
+        # TODO: Receive a response from the server and close the TCP connection
         data = s.recv(1024) # read a maximum of 1024 bytes
     print("Received:", repr(data))
     s.close()
-
-    # TODO: Create a socket and connect it to the server at the designated IP and port
-
-
-    # TODO: Get user input and send it to the server using your TCP socket
-
-
-    # TODO: Receive a response from the server and close the TCP connection
-
-
-
 
 if __name__ == '__main__':
     main()
